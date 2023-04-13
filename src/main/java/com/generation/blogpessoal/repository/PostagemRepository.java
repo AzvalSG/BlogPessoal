dda92b3d85b8e70a06d4149c03b65bbe6e7f17dc
AzvalSG <127205710+AzvalSG@users.noreply.github.com>
2023-04-13 15:19:40 -0300

    Update PostagemRepository.java

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -2,7 +2,7 @@ package com.generation.blogpessoal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.generation.blogpessoal.model.Postagem;
-
+import org.springframework.stereotype.Repository;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
