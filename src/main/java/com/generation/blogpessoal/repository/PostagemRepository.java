b06e212ef4becf855f226afbeea6a5c445af7f59
AzvalSG <127205710+AzvalSG@users.noreply.github.com>
2023-04-14 12:52:47 -0300

    Atualização
    Inserção das funções GET, POST, PUT e DELETE de postagem.

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,18 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.generation.blogpessoal.model.Postagem;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.generation.blogpessoal.model.Postagem;
+<<<<<<< HEAD
+
+=======
+import org.springframework.stereotype.Repository;
+>>>>>>> dda92b3d85b8e70a06d4149c03b65bbe6e7f17dc
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
-
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
